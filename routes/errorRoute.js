e1b6a632774dd49368b54c7ca752ac6276f8635c
anacarol-1808 <126124189+anacarol-1808@users.noreply.github.com>
2024-10-03 21:57:04 -0600

    Image error working

--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -5,4 +5,4 @@ const errorController = require("../controllers/errorController")
 const utilities = require("../utilities")
 
 // Week 03 - Route to build inventory detail Error
-router.get("/error/", utilities.handleErrors(errorController.buildIntentionalError))
+//router.get("/error/", utilities.handleErrors(errorController.buildIntentionalError))
--- server.js
+++ server.js
@@ -41,12 +41,13 @@ app.use(async (req, res, next) => {
 *************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
+  let errorImage = ""
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
   if(err.status == 404){ 
     message = err.message
   } else {
     message = 'Oh no! There was a crash. Maybe try a different route?'
-    errorImage = '/images/site/crash.jpg'
+    errorImage = '<img src="/images/site/crash.jpg" alt="carCrash">'
   }
   res.render("errors/error", {
     title: err.status || 'Server Error',
