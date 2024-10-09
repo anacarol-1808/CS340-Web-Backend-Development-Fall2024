4109c890d59a3e83ab2207b9efc4160e4272923c
anacarol-1808 <126124189+anacarol-1808@users.noreply.github.com>
2024-10-09 14:16:31 -0600

    client side validation and server side validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController");
+const regValidate = require('../utilities/account-validation')
 
 // Week 04 - "GET" route for the path that will be sent when the "My Account" link is clicked
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -11,6 +12,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 // Week 04 - Route to Process the Registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data (week 04) - using the server validation tools
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 module.exports = router;
