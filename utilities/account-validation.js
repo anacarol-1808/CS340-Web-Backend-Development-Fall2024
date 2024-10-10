7bbe6f02133079632f7c7b78e491f7317aaee20a
anacarol-1808 <126124189+anacarol-1808@users.noreply.github.com>
2024-10-10 14:53:04 -0600

    Week 04 - Server data validation for Login View and Process is implemented

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,6 +1,7 @@
 const utilities = require(".")
 const { body, validationResult } = require("express-validator")
 const validate = {}
+const accountModel = require("../models/account-model")
 
 /*  **********************************
   *  Registration Data Validation Rules (week 04)
@@ -23,14 +24,18 @@ validate.registationRules = () => {
         .isLength({ min: 2 })
         .withMessage("Please provide a last name."), // on error this message is sent.
   
-      // valid email is required and cannot already exist in the DB
+      // valid email is required and cannot already exist in the database
       body("account_email")
       .trim()
-      .escape()
-      .notEmpty()
       .isEmail()
       .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+      .withMessage("A valid email is required.")
+      .custom(async (account_email) => {
+        const emailExists = await accountModel.checkExistingEmail(account_email)
+        if (emailExists){
+          throw new Error("Email exists. Please log in or use different email")
+        }
+      }),
   
       // password is required and must be strong password
       body("account_password")
@@ -68,5 +73,53 @@ validate.checkRegData = async (req, res, next) => {
     }
     next()
   }
+
+/* **********************************
+*  Login Data Validation Rules (week 04)
+* ********************************* */
+validate.loginRules = () => {
+  return [
+    // valid email is required
+    body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("A valid email is required."),
+
+    // password is required
+    body("account_password")
+      .trim()
+      .notEmpty()
+      .isStrongPassword({
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("Password is required and must meet the requirements: Passwords must be at least 12 characters and contain at least 1 number, 1 capital letter and 1 special character."),
+  ];
+};
   
+
+/* ******************************
+ * Check data and return errors or continue to login (week 04) --- NOT SURE ABOUT THIS BEING REQUIRED OR NOT
+ * ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+  const { account_email, account_password } = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      errors,
+      title: "Login",
+      nav,
+      account_email,
+      account_password
+    })
+    return
+  }
+  next()
+}
   module.exports = validate
